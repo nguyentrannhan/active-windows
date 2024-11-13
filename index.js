d2a0fb7c582f5e0596d5da7549010f6283086f5a
Nhan Nguyen <nguyentran@ncc.asia>
2024-11-13 10:21:12 +0700

    update version

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-const activeWindows = require('./build/Release/wm');
 
-module.exports = activeWindows;
+if (process.platform === 'darwin' && process.arch === 'x64') {
+  module.exports = {};
+} else {
+  const activeWindows = require('./build/Release/wm');
+  module.exports = activeWindows;
+}
