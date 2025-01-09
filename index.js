b2e5703ec18f3dcb56d725d4584add3f0cdf6368
NccSoft Vietnam <nguyentran@ncc.asia>
2025-01-09 19:33:04 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,2 @@
-
-if (process.platform === 'darwin' && (process.arch === 'x64' || process.arch === 'x32')) {
-  module.exports = {};
-} else {
-  const activeWindows = require('./build/Release/wm.node');
-  module.exports = activeWindows;
-}
+const activeWindows = require('./build/Release/wm.node');
+module.exports = activeWindows;
