b916b59200429a9214d161103db5726b18a8d361
Nhan Nguyen <nguyentran@ncc.asia>
2025-01-09 19:38:50 +0700

    fix check arch

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-const activeWindows = require('./build/Release/wm.node');
-module.exports = activeWindows;
+try {
+  const activeWindows = require('./build/Release/wm.node');
+  module.exports = activeWindows;
+} catch (ex) {
+  module.exports = {}
+}
